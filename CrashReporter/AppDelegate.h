b941a213b3e270448c48ddec58c3a94a438c4d69
c <phnessu4@gmail.com>
2012-10-22 15:00:52 +0800

    Initial Commit

---
+++ CrashReporter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CrashReporter
+//
+//  Created by c on 12-10-22.
+//  Copyright (c) 2012年 stylejar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CrashReporter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CrashReporter
+//
+//  Created by c on 12-10-22.
+//  Copyright (c) 2012年 stylejar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
